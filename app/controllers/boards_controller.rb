2bed13f9ce86870c3a411024c86b1909ae4a9142
will Liu <wye139629@gmail.com>
2020-07-24 16:36:30 +0800

    modified edit, add validate and scss

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,22 +1,45 @@
 class BoardsController < ApplicationController
+
+
   def index
     @boards = Board.all
   end
+
+  def show
+      @board = Board.find(params[:id])  
+  end
+
   def new
     @board = Board.new
   end
   def create
-    render html: params
-    # @board = Board.new(params_board)
-    # if @board.save
-    #   redirect boards_path, notice: "新增成功"
-    # else
-    #   render :new
-    # end
+    # render html: params
+    @board = Board.new(params_board)
+    if @board.save
+      redirect_to boards_path, notice: "新增成功"
+    else
+      render :new
+    end
   end
 
   def edit
-    @board = Board.find_by(params[:id])
+    @board = Board.find_by(id: params[:id])
+  end
+
+  def update
+    @board = Board.find_by(id: params[:id])
+    if @board.update(params_board)
+      redirect_to boards_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board = Board.find_by(id: params[:id])
+    if @board.destroy
+    redirect_to boards_path , notice: "刪除成功"
+    end
   end
 
   private
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,4 @@
 class Board < ApplicationRecord
+  validates :title, presence: true
+  
 end
